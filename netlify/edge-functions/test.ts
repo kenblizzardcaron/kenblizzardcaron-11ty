4031e38a8e714d5fa673bf54e33bcefca053f9fc
Ken Blizzard-Caron <ken.blizzardcaron@gmail.com>
2024-06-15 22:03:34 -0500

    debug

--- netlify/edge-functions/test.ts
+++ netlify/edge-functions/test.ts
@@ -1,14 +1,14 @@
 //import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
-import puppeteer from "npm:puppeteer-core";
+//import puppeteer from "npm:puppeteer-core";
 //const puppeteer = require('puppeteer-core');
 import { emojify } from "npm:node-emoji@2";
 
 console.log(emojify(":t-rex: :heart: NPM"));
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto("https://kenblizzardcaron.com/resume", {
-  waitUntil: "networkidle2",
-});
-await page.pdf({ path: "hn.pdf", format: "A4" });
+// const browser = await puppeteer.launch();
+// const page = await browser.newPage();
+// await page.goto("https://kenblizzardcaron.com/resume", {
+//   waitUntil: "networkidle2",
+// });
+// await page.pdf({ path: "hn.pdf", format: "A4" });
 
-await browser.close();
+// await browser.close();
